7ceb71a67aaa7c6404ac8abc7c925002ed65b3b8
Kelvin Jayanoris <kjayanoris@gmail.com>
2020-09-22 12:37:37 +0300

    Update structure geo hierarchy

--- 3-reveal/jobs/materialized-views/IRS/Zambia-2019/process_structure_geo_hierarchy_jurisdiction_queue.sql
+++ 3-reveal/jobs/materialized-views/IRS/Zambia-2019/process_structure_geo_hierarchy_jurisdiction_queue.sql
@@ -1,3 +1,4 @@
 -- Suggestion is to refresh this every 1 day
 -- This is reprocesses all structures and is most probably the most expensive thing you can do
-SELECT process_structure_geo_hierarchy_jurisdiction_queue();
+SELECT refresh_mat_view('structure_geo_hierarchy', TRUE);
+SELECT refresh_mat_view('structures_geo_jurisdictions', TRUE);
